d350d1a9c067824bea5658e031b8d0bf501f9afa
Vinícius Chiarato <viniciussantoschiarato@gmail.com>
2022-05-19 16:52:31 -0300

    Primeiro Commit
    Exercícios do 1 ao 5

---
+++ Exercícios Completos/ex003.py
@@ -0,0 +1,5 @@
+print('Programa que faz soma de numeros inteiros')
+vr1 = int(input('Digite um número: '))
+vr2 = int(input('Digite outro número: '))
+vr3 = vr1+vr2
+print('A soma entre {} e {} resulta em {}!'.format(vr1, vr2, vr3))
